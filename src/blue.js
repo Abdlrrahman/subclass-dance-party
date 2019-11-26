3bfa0da379361968d44c18f936d40b92410cd1c2
Abdlrrahman <56532938+Abdlrrahman@users.noreply.github.com>
2019-11-26 19:08:15 +0100

    2

--- src/blue.js
+++ src/blue.js
@@ -1,6 +1,6 @@
-var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+var MakeBlinkyDancerBlue = function(top, left, timeBetweenSteps) {
  // var blinkyDancer = makeDancer(top, left, timeBetweenSteps)
- MakeDancer.call(this, top, left, timeBetweenSteps)
+ MakeDancerBlue.call(this, top, left, timeBetweenSteps)
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -8,13 +8,13 @@ var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
   
   };
 
-  MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
-  MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
+  MakeBlinkyDancerBlue.prototype = Object.create(MakeDancerBlue.prototype);
+  MakeBlinkyDancerBlue.prototype.constructor = MakeBlinkyDancerBlue;
   
-  MakeBlinkyDancer.prototype.step = function() {
+  MakeBlinkyDancerBlue.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-    var that = MakeDancer.prototype.step;
-    that.call(this);
+    // var that = MakeDancerBlue.prototype.step;
+    MakeDancerBlue.prototype.step.call(this);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
